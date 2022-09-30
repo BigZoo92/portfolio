acb99c126ffdce74aa044e0ea9b2dc50a759e4b3
BigZoo92 <givernaudenzo@gmail.com>
2022-09-30 16:31:22 +0200

    toggle

--- assets/js/script/script.js
+++ assets/js/script/script.js
@@ -1,15 +1,26 @@
-document.querySelector('.menu_toggle').addEventListener("click", menuToggle);
-
-function menuToggle() {
-  console.log('wesh');
-  const tlToggle = gsap.timeline({});
-  tlToggle.to('.toggle_arm1', {y: -7, ease: 'back'})
+document.querySelector('.menu_toggle').addEventListener("click", choiceToggle);
+let choice = true;
+const tlToggle = gsap.timeline({paused : true})
+tlToggle.to('.toggle_arm1', {y: -7, ease: 'back'})
           .to('.toggle_arm2', {y: 20, ease: 'back'}, '<')
           .to('.menu', {'clip-path' : 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)'}, '<')
           .to('.toggle_arm1', {x: -55, ease: 'power4'})
           .to('.toggle_arm2', {x: 55, ease: 'power4'}, '<')
           .to('.close', {'clip-path' : 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)'}, '<')
-  
+          .to('.toggle_arm1', {x: 0, ease: 'power4'})
+          .to('.toggle_arm2', {x: 0, ease: 'power4'})
+function choiceToggle(){
+  choice ? menuToggle() : menuToggleBis();
+}
+function menuToggle() {
+  choice = false;
+  console.log(choice);
+  tlToggle.play()
+}
+function menuToggleBis() {
+  choice = true;
+  console.log(choice);
+  tlToggle.reverse();
 }
 
 let tlHero = gsap.timeline({ defaults: { ease: "back", duration: 0.75, stagger: 0.03} })
