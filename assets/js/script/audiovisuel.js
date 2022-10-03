202f3670dc2748033e5eec5d8d3e5208c907878e
BigZoo92 <givernaudenzo@gmail.com>
2022-10-04 00:11:18 +0200

    grainy effect

--- assets/js/script/audiovisuel.js
+++ assets/js/script/audiovisuel.js
@@ -2,10 +2,10 @@ const options = {
     "animate": true,
     "patternWidth": 10,
     "patternHeight": 500,
-    "grainOpacity": 0.05,
-    "grainDensity": 3,
+    "grainOpacity": 0.02,
+    "grainDensity": 5,
     "grainWidth": 1,
-    "grainHeight": 2
+    "grainHeight": 5
 }
 
 grained('#container_image_audiovisuel', options);
