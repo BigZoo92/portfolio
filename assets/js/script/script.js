cad31bb2486d9ea8e4c0584d0cd7a9495bb6edd5
BigZoo92 <givernaudenzo@gmail.com>
2022-10-03 20:33:56 +0200

    final header

--- assets/js/script/script.js
+++ assets/js/script/script.js
@@ -15,12 +15,10 @@ function choiceToggle(){
 }
 function menuToggle() {
   choice = false;
-  console.log(choice);
   tlToggle.play()
 }
 function menuToggleBis() {
   choice = true;
-  console.log(choice);
   tlToggle.reverse();
 }
 
@@ -28,4 +26,63 @@ let tlHero = gsap.timeline({ defaults: { ease: "back", duration: 0.75, stagger: 
 tlHero.from('.letter1', {y: 300})
       .from('.letter2', {y: 300}, "<0.2")
 
-      
+let tlHoverToggle = gsap.timeline({paused : true, defaults: { ease: "sine", duration: 0.2} })
+tlHoverToggle.to('.toggle_arm1', {'width': '50%'})
+              .to('.toggle_arm2', {'width': '100%'}, "<")
+function toggleMOver() {
+  tlHoverToggle.play()
+}
+function toggleMOut() {
+  tlHoverToggle.reverse()
+}
+
+let tlOverLi1 = gsap.timeline({paused : true, defaults: { ease: "sine", duration: 0.5, stagger: 0.05} })
+tlOverLi1.to('.li_letter11', {y: -100})
+         .to('.li_letter12', {y: -100}, "<")
+
+function liOver1(){
+  tlOverLi1.play()
+  $( ".li_letter21" ).css("opacity", "0.5")
+  $( ".li_letter31" ).css("opacity", "0.5")
+  $( ".i1" ).css("opacity", "1")
+}
+function liOut1(){
+  tlOverLi1.reverse()
+  $( ".li_letter21" ).css("opacity", "1")
+  $( ".li_letter31" ).css("opacity", "1")
+  $( ".i1" ).css("opacity", "0.5")
+}
+
+let tlOverLi2 = gsap.timeline({paused : true, defaults: { ease: "sine", duration: 0.5, stagger: 0.05} })
+tlOverLi2.to('.li_letter21', {y: -100})
+         .to('.li_letter22', {y: -100}, "<")
+
+
+function liOver2(){
+  tlOverLi2.play()
+  $( ".li_letter11" ).css("opacity", "0.5")
+  $( ".li_letter31" ).css("opacity", "0.5")
+  $( ".i2" ).css("opacity", "1")
+}
+function liOut2(){
+  tlOverLi2.reverse()
+  $( ".li_letter11" ).css("opacity", "1")
+  $( ".li_letter31" ).css("opacity", "1")
+  $( ".i2" ).css("opacity", "0.5")
+}
+
+let tlOverLi3 = gsap.timeline({paused : true, defaults: { ease: "sine", duration: 0.5, stagger: 0.05} })
+tlOverLi3.to('.li_letter31', {y: -100})
+         .to('.li_letter32', {y: -100}, "<")
+function liOver3(){
+  tlOverLi3.play()
+  $( ".li_letter11" ).css("opacity", "0.5")
+  $( ".li_letter21" ).css("opacity", "0.5")
+  $( ".i3" ).css("opacity", "1")
+}
+function liOut3(){
+  tlOverLi3.reverse()
+  $( ".li_letter11" ).css("opacity", "1")
+  $( ".li_letter21" ).css("opacity", "1")
+  $( ".i3" ).css("opacity", "0.5")
+}
