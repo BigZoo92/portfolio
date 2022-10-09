2252cd2c180925cb7c1bb7954228d1d2c9c70109
BigZoo92 <givernaudenzo@gmail.com>
2022-10-09 13:32:52 +0200

    text scrolling

--- assets/js/script/audiovisuel.js
+++ assets/js/script/audiovisuel.js
@@ -1,39 +1,57 @@
 gsap.registerPlugin(ScrollTrigger);
-function scrollAudioviuelProject(){
-    ScrollTrigger.refresh(true)
-    console.log('wesh');
-}
-let tlAudioProject = gsap.timeline({defaults: { ease: "power4", duration: 0.5 } })
-gsap.utils.toArray(".numFilm").forEach((numFilm, i) => {
-    tlAudioProject.to(numFilm, { y: -100 })
-})
+
+let tlAudioProject = gsap.timeline({ defaults: { ease: "back", duration: 0.5 } })
 tlAudioProject.to('.audiovisuel_chiffre_1', { y: -100 })
-    .to('.audiovisuel_chiffre_2', { y: -100 }, "<")
-    .to('.audiovisuel_chiffre_3', { y: -100 }, "<")
-    .to('.titre1 h2', { y: '-20vh', stagger: 0.03 }, "<")
-    .to('.titre2 h2', { y: '-20vh', stagger: 0.03 }, "<")
-    .to('.titre3 h2', { y: '-2Ovh', stagger: 0.03 }, "<")
-    .to('.docu', { y: '-30vh' }, "<")
-    .to('.pub', { y: '-35vh' }, "<")
-    .to('.motion', { y: '-40vh' }, "<")
+  .to('.audiovisuel_chiffre_2', { y: -100 }, "<")
+  .to('.titre1 h2', { y: '-20vh', stagger: 0.03 }, "<")
+  .to('.titre2 h2', { y: '-20vh', stagger: 0.03 }, "<")
+  .to('.docu', { y: '-30vh' }, "<")
+  .to('.pub', { y: '-35vh' }, "<")
+  .to('.motion', { y: '-25vh' }, "<")
+let tlAudioProjectBis = gsap.timeline({ defaults: { ease: "back", duration: 0.5 } })
+tlAudioProjectBis.to('.audiovisuel_chiffre_2', { y: -100 }, "<")
+  .to('.audiovisuel_chiffre_3', { y: -100 }, "<")
+  .to('.titre2 h2', { y: '-20vh', stagger: 0.03 }, "<")
+  .to('.titre3 h2', { y: '-2Ovh', stagger: 0.03 }, "<")
+  .to('.docu', { y: '-30vh' }, "<")
+  .to('.pub', { y: '-35vh' }, "<")  
+  .to('.motion', { y: '-25vh' }, "<")
 ScrollTrigger.create({
-    animation: tlAudioProject,
-        trigger: '.audiovisuel_project',
-        start: "top top",
-        pin: true,
-        markers: true,
-        snap: {
-            snapTo: 1,
-            duration: 1,
-            delay: 0
-          },
-        onLeave: self => {
-            self.scroll(self.start + 1);
-            ScrollTrigger.update();
-          },
-          onLeaveBack: self => {
-            self.scroll(self.end - 1);
-            ScrollTrigger.update();
-          }
+  animation: tlAudioProject,
+  trigger: '.audiovisuel_project',
+  start: "1px",
+  markers: true,
+  end: 'max',
+  pin: true,
+  onEnter: self => {
+    $('.titre2 h2').css("display", "block")
+    $('.audiovisuel_chiffre_2').css("display", "block")
+  },
+  onLeaveBack: self => {
+    $('.titre2 h2').css("top", "20vh")
+    $('.audiovisuel_chiffre_2').css("top", "100px")
+    tlAudioProject.reverse()
+  },
+
+})
+ScrollTrigger.create({
+  animation: tlAudioProjectBis,
+  trigger: '.marker',
+  start: "bottom",
+  end: 'max',
+  pin: true,
+  markers: true,
+  onEnter: self => {
+    $('.audiovisuel_chiffre_2').css("display", "none")
+    $('.titre2 h2').css("display", "block")
+    console.log('wesh');
+  },
+  onLeaveBack: self => {
+    $('.titre2 h2').css("display", "block")
+    $('.titre2 h2').css("display", "top", "0")
+    $('.audiovisuel_chiffre_2').css("display", "block")
+    $('.audiovisuel_chiffre_2').css("top", "0")
+    tlAudioProjectBis.reverse()
+  }
 })
 
--- assets/js/script/home.js
+++ assets/js/script/home.js
@@ -1,3 +1,20 @@
-let tlHero = gsap.timeline({ defaults: { ease: "back", duration: 0.75, stagger: 0.03} })
-tlHero.from('.letter1', {y: 300})
-    .from('.letter2', {y: 300}, "<0.2")
+let tlHeroTest = gsap.timeline({ defaults: { ease: "power4", duration: 0.75, stagger: 0.03} })
+tlHeroTest.to('.overlay_hero', {'width': '0%', 'left': '25%'})
+          .to('.overlay_hero', {'width': '50%', 'left': '100%'}, "<0.1")
+          .to('.enzo_creative h1', {'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)'}, "<")
+          .to('.givernaud_developer h1', {'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)'}, "<0.1")
+let tlHeroBis = gsap.timeline({ defaults: { ease: "none", duration: 1} })
+tlHeroBis.to('.enzo', {y: -200})
+         .to('.creative', {y: -165}, "<")
+         .to('.givernaud', {y: -200}, "<")
+         .to('.developer', {y: -165}, "<")
+ScrollTrigger.create({
+   animation: tlHeroBis,
+   trigger: '.hero',
+   start: "1px",
+   markers: false,
+   end: 'max',
+   pin: true,
+   scrub: true,
+ })
+
